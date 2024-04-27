b51b1249496d3f561448c8291ff71fcfdc7dcac6
Nikita Maslenko <nickita0098@gmail.com>
2024-04-27 15:56:30 +0200

    Реализуется модель товаров и категорий

--- shop_backend/goods/admin.py
+++ shop_backend/goods/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Characteristic, Goods
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Characteristic)
+class CharacteristicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Goods)
+class GoodsAdmin(admin.ModelAdmin):
+    pass
+    list_display = ('vendor_code', 'name', 'category', 'price', 'description', 'get_characteristic')
+    search_fields = ('name',)
+    list_filter = ('price', 'characteristic')
+
+    def get_characteristic(self, obj):
+        return ', '.join([one_characteristic.name for one_characteristic in obj.characteristic.all()])
--- shop_backend/goods/models.py
+++ shop_backend/goods/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+SHORT_NAME_LENGTH = 10
+
+
+class Category(models.Model):
+    name = models.CharField('Название', max_length=128)
+    slug = models.SlugField('Слаг', unique=True, blank=True)
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+
+class Characteristic(models.Model):
+    name = models.CharField('Название', max_length=64, unique=True)
+
+    class Meta:
+        verbose_name = 'характеристика'
+        verbose_name_plural = 'Характеристики'
+
+    def __str__(self):
+        return self.name[:SHORT_NAME_LENGTH]
+
+
+class Goods(models.Model):
+    vendor_code = models.CharField('Артикул', max_length=64, unique=True)
+    name = models.CharField('Название', max_length=128)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='goods', verbose_name='Категория')
+    slug = models.SlugField('Слаг', unique=True, blank=True)
+    price = models.DecimalField('Цена', max_digits=9, decimal_places=2)
+    description = models.TextField('Описание', max_length=2000)
+    characteristic = models.ManyToManyField(Characteristic, related_name='goods', verbose_name='Характеристики')
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name[:SHORT_NAME_LENGTH]
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.name)
+        super(Goods, self).save(*args, **kwargs)
